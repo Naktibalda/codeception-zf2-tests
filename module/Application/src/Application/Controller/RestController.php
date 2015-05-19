66575ad920ff9cd632fdd3b834d071d63be0caf6
Gintautas Miselis <gintautas@miselis.lt>
2015-05-19 23:18:00 +0100

    Commited commented out code

--- module/Application/src/Application/Controller/RestController.php
+++ module/Application/src/Application/Controller/RestController.php
@@ -18,13 +18,7 @@ class RestController extends AbstractActionController
     public function indexAction()
     {
         $request = $this->getServiceLocator()->get('Request');
-    /*
-    $this->params()->fromPost('paramname');   // From POST
-$this->params()->fromQuery('paramname');  // From GET
-$this->params()->fromRoute('paramname');  // From RouteMatch
-$this->params()->fromHeader('paramname'); // From header
-$this->params()->fromFiles('paramname');  // From file being uploaded
-*/
+    
         $data = array(
             'requestMethod' => $request->getMethod(),
             'requestUri' => $request->getRequestUri(),
