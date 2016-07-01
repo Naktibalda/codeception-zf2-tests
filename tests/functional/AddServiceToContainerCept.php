55635c59296f15c00746ed40669e05c44fd6845a
Gintautas Miselis <gintautas@miselis.lt>
2016-07-01 22:49:46 +0100

    Removed link to incorrect issue

--- tests/functional/AddServiceToContainerCept.php
+++ tests/functional/AddServiceToContainerCept.php
@@ -1,9 +1,7 @@
 <?php
-/**
- * @issue https://github.com/Codeception/Codeception/issues/2814
- */
+
 $I = new FunctionalTester($scenario);
-$I->wantTo('see that added service persist between requests');
+$I->wantTo('see that added service persists between requests');
 
 $I->addServiceToContainer('foo', (object)['bar' => 1]);
 $I->amOnPage('/form');
