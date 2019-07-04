023afddad40585b3e96dfec06d3ee1802be185a1
Vytautas Stankus <svycka@gmail.com>
2019-07-04 16:56:28 +0300

    whoops

--- tests/doctrine/createUserCept.php
+++ tests/doctrine/createUserCept.php
@@ -2,11 +2,12 @@
 $I = new DoctrineTester($scenario);
 $I->wantTo('check that the user was persisted');
 
-//$I->dontSeeInRepository('\Application\Entity\User', ['name' => 'John Doe']);
-//
-//$I->amOnPage('/doctrine/');
-//$I->seeResponseCodeIs(200);
-//$I->fillField('name', 'John Doe');
-//$I->click('Create User');
-//
-//$I->seeInRepository('\Application\Entity\User', ['name' => 'John Doe']);
+$I->dontSeeInRepository('\Application\Entity\User', ['name' => 'John Doe']);
+
+$I->amOnPage('/doctrine/');
+$I->seeResponseCodeIs(200);
+$I->fillField('name', 'John Doe');
+$I->click('Create User');
+$I->see('1 users in the table');
+
+$I->seeInRepository('\Application\Entity\User', ['name' => 'John Doe']);
